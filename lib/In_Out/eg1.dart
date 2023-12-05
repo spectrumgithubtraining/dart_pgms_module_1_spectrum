282676703fc101b1137aa7e62b34b04573d48d03
Your Name <spectrumgithubtraining>
2023-12-05 09:08:34 +0530

    done

--- lib/In_Out/eg1.dart
+++ lib/In_Out/eg1.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 void main() {
   int num1, num2, result;
-  stdout.write("Enter first number");
+  stdout.write("Enter first number \n");
   num1 = int.parse(stdin.readLineSync()!);
-  stdout.write("Enter second number");
+  stdout.write("Enter second number \n");
   num2 = int.parse(stdin.readLineSync()!);
   result = num1 + num2;
   stdout.write("Sum : $result");
